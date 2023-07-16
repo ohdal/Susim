2ab28d168302ad7671651fa613a7974e24cc2d23
윤현정 <yoonhj97@naver.com>
2023-07-16 22:05:57 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import IntroPage from "./pages/IntroPage";
+import MainPage from "./pages/mainPage";
+import ArchivePage from "./pages/archivePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IntroPage />,
+  },
+  {
+    path: "/main",
+    element: <MainPage />,
+  },
+  {
+    path: "/archive",
+    element: <ArchivePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
