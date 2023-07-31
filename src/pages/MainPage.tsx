afc12a0629f21ede37fcdb3260302eed2fb4e56e
윤현정 <yoonhj97@naver.com>
2023-07-31 12:44:23 +0900

    fix(MainPage): 에러 수정 & asyn/await 으로 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,35 +1,38 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useCallback, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function MainPage() {
   const navigate = useNavigate();
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  useEffect(() => {
+  const getMediaStream = useCallback(async () => {
     // const AudioContext = window.AudioContext || window.webkitAudioContext;
     const AudioContext = window.AudioContext;
     const audioContext = new AudioContext();
     const analyser = audioContext.createAnalyser();
+    let mediaStream = null;
 
+    // https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode
     // 마이크 MediaStream 취득(audio를 true로 설정)
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((mediaStream) => {
-        // Promise이므로 then으로 받아서 사용(async/await를 사용해도 된다.)
+    try {
+      mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      if (audioRef.current && mediaStream) {
+        // 마이크에서 입력받은 MediaStream으로 오디오 재생
+        audioRef.current.srcObject = mediaStream;
 
-        if (audioRef.current) {
-          // 마이크에서 입력받은 MediaStream으로 오디오 재생
-          audioRef.current.srcObject = mediaStream;
-
-          const source = audioContext.createMediaElementSource(audioRef.current);
-          source.connect(analyser);
-          // 데이터 시각화...
-        }
-      })
-      .catch((err: string) => {
-        console.error(`에러 발생: ${err}`);
-      });
+        const source = audioContext.createMediaElementSource(audioRef.current);
+        source.connect(analyser);
+        // 데이터 시각화...
+      }
+      /* use the stream */
+    } catch (err) {
+      console.error(`에러 발생: ${String(err)}`);
+    }
   }, []);
+
+  useEffect(() => {
+    void getMediaStream();
+  }, [getMediaStream]);
 
   return (
     <div>
