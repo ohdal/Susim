3e7c904e4dd0a7885dec45cd031516dddddc4bd5
윤현정 <yoonhj97@naver.com>
2023-07-31 15:59:52 +0900

    feat(scatter): scatter 애니메이션 text 여러줄

--- src/components/ScatterCanvas.tsx
+++ src/components/ScatterCanvas.tsx
@@ -14,11 +14,17 @@ const PointerDiv = styled.div<{ $width: number; $height: number }>`
   width: ${(props) => props.$width}px;
   height: ${(props) => props.$height}px;
   cursor: pointer;
+  // background: rgba(201, 131, 131, 0.5);
   background: rgba(255, 255, 255, 0);
 `;
 
 type Props = {
-  text: string;
+  text: string[];
+};
+
+type PointerDiv = {
+  width: number;
+  height: number;
 };
 
 class Particle {
@@ -32,10 +38,13 @@ class Particle {
   private ctx: CanvasRenderingContext2D;
 
   constructor(x: number, y: number, opacity: number, ctx: CanvasRenderingContext2D) {
+    const r = getRandomInt(0, 1);
+    const angle = (Math.PI / 180) * getRandomInt(0, 360);
+
     this.x = x;
     this.y = y;
-    this.firstPosX = getRandomInt(-1, 2);
-    this.firstPosY = getRandomInt(-1, 2);
+    this.firstPosX = r * Math.cos(angle);
+    this.firstPosY = r * Math.sin(angle);
     this.speed = 1;
     this.friction = 0.9;
 
@@ -48,7 +57,7 @@ class Particle {
     this.y += this.firstPosY * this.speed;
 
     this.speed *= this.friction;
-    this.opacity -= 0.02;
+    this.opacity -= 0.025;
   }
 
   update(): void {
@@ -58,34 +67,36 @@ class Particle {
   draw() {
     this.ctx.beginPath();
     this.ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
-    // ctx.arc((count % width) + 100, Math.floor((count / width)) + 100, 1, 0, (Math.PI / 180) * 360);
-    // ctx.fill();
+    // this.ctx.arc(this.x, this.y, 1, 0, (Math.PI / 180) * 360);
+    // this.ctx.fill();
     this.ctx.fillRect(this.x, this.y, 1, 1);
     this.ctx.closePath();
   }
 }
 
 const particles: { [key: string]: Particle } = {};
+const fontSize = 32;
 export default function ScatterCanvas(props: Props) {
   const { text } = props;
   const navigate = useNavigate();
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [canvas, setCanvas] = useState<Canvas | null>(null);
-  const [pointerDiv, setPointerDiv] = useState<{ width: number; height: number } | null>(null);
+  const [pointerDiv, setPointerDiv] = useState<PointerDiv | null>(null);
 
   const createParticle = useCallback(
-    (text: string, xPos: number, yPos: number) => {
+    (textInfo: PointerDiv, xPos: number, yPos: number) => {
       if (!canvas || !canvas.ctx) return;
 
       // particles = {};
 
       const ctx = canvas.ctx;
       const dpr = canvas.dpr;
-      const met = ctx.measureText(text);
+      const met = textInfo;
       const width = Math.floor(met.width * dpr);
 
-      const imgData = ctx.getImageData(xPos * dpr - met.width, yPos * dpr - 40, met.width * dpr, 40 * dpr);
+      const imgData = ctx.getImageData(xPos * dpr - met.width, yPos * dpr, met.width * dpr, met.height * dpr);
+
       canvas.clearCanvas();
 
       let count = 0;
@@ -93,13 +104,17 @@ export default function ScatterCanvas(props: Props) {
         count++;
 
         if (imgData.data[i + 3] !== 0) {
-          const x = (count % (width / 2)) + (xPos - met.width / 2); // 100 임시
-          const y = Math.floor(count / width) + (yPos - 19);
+          const x = (count % (width / 2)) + (xPos - met.width / 2);
+          const y = Math.floor(count / width) + yPos;
+          const coordX = Math.floor(x);
+          const coordY = Math.floor(y);
 
-          particles[`${x}-${y}`] = new Particle(x - 1, y, imgData.data[i + 3] / 255, ctx);
-          particles[`${x}-${y}`].draw();
+          particles[`${coordX}-${coordY}`] = new Particle(x - 1, y, imgData.data[i + 3] / 255, ctx);
+          particles[`${coordX}-${coordY}`].draw();
         }
       }
+
+      console.log(particles);
     },
     [canvas]
   );
@@ -108,8 +123,10 @@ export default function ScatterCanvas(props: Props) {
     if (!canvas) return;
 
     const ctx = canvas.ctx as CanvasRenderingContext2D;
+    const totalHeight = text.length * fontSize;
+    const spacing = canvas.dpr * text.length;
     const x = canvas.CANVAS_WIDTH / 2;
-    const y = canvas.CANVAS_HEIGHT / 2;
+    const y = (canvas.CANVAS_HEIGHT - totalHeight) / 2 + spacing;
 
     // const img: HTMLImageElement = new Image();
     // img.src = text_img;
@@ -142,57 +159,92 @@ export default function ScatterCanvas(props: Props) {
     // ctx.putImageData(imgData, 100, 100);
     // };
 
-    ctx.beginPath();
-    ctx.fillStyle = "#FFFFFF";
-    ctx.font = "36px san-serif";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText(text, x, y);
-    ctx.closePath();
+    let met;
+    let maxWidth = 0;
+    for (let i = 0; i < text.length; i++) {
+      ctx.beginPath();
+      ctx.fillStyle = "#FFFFFF";
+      ctx.font = `${fontSize}px san-serif`;
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(text[i], x, y + fontSize * i);
 
-    const met = ctx.measureText(text);
-    setPointerDiv({ width: met.width, height: 40 });
+      met = ctx.measureText(text[i]);
+      if (!maxWidth) maxWidth = met.width;
+      else if (maxWidth < met.width) maxWidth = met.width;
+    }
+
+    setPointerDiv({ width: maxWidth, height: totalHeight });
   }, [canvas, text]);
 
-  const animation = useCallback((isFirst = false): boolean => {
-    if (Object.keys(particles).length === 0) {
-      navigate("/main");
-      return false;
-    }
-
-    for (const [key, value] of Object.entries(particles)) {
-      value.draw();
-      if (isFirst) {
-        value.firstUpdate();
-
-        if (value.opacity <= 0) delete particles[key];
-        // if (value.speed <= 0) {
-        //   return false;
-        // }
-      } else {
-        value.update();
-
-        if (value.opacity <= 0) delete particles[key];
+  const animation = useCallback(
+    (isFirst: boolean, x?: number, y?: number): boolean => {
+      if (Object.keys(particles).length === 0) {
+        navigate("/main");
+        return false;
       }
-    }
 
-    return true;
-  }, []);
+      for (const [key, value] of Object.entries(particles)) {
+        if (isFirst) {
+          value.draw();
+          value.firstUpdate();
 
-  const handleCanvas = useCallback((e: MouseEvent<HTMLCanvasElement>) => {
-    console.log(e.clientX, e.clientY);
-  }, []);
+          if (value.opacity <= 0) {
+            delete particles[key];
+          }
+          // if (value.speed <= 0.001) {
+          // return false;
+          // }
+        } else {
+          if (!x || !y) return false;
+          value.draw();
+
+          const coord = `${x}-${y}`;
+          const particle = particles[coord];
+
+          if (particle) {
+            console.log(particle);
+            for (let i = 0; i < 5; i++) {
+              const coord = `${x - i}-${y - i}`;
+              const coord2 = `${x + i}-${y + i}`;
+              const other = particles[coord];
+              const other2 = particles[coord2];
+
+              if (other) other.update();
+              if (other2) other2.update();
+            }
+          } else {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    },
+    [navigate]
+  );
+
+  const handleCanvas = useCallback(
+    (e: MouseEvent<HTMLCanvasElement>) => {
+      if (!canvas) return;
+
+      console.log(e.clientX, e.clientY);
+
+      // canvas.animate(() => {
+      //   return animation(false, e.clientX, e.clientY);
+      // });
+    },
+    [canvas, animation]
+  );
 
   const handlePointerDiv = useCallback(() => {
-    if (!canvas) return;
+    if (!canvas || !pointerDiv) return;
 
-    createParticle(text, canvas.CANVAS_WIDTH / 2, canvas.CANVAS_HEIGHT / 2);
-    setPointerDiv(null);
-
-    canvas.animate(() => {
-      return animation(true);
-    });
-  }, [canvas, text, createParticle, animation]);
+    const x = canvas.CANVAS_WIDTH / 2;
+    const y = (canvas.CANVAS_HEIGHT - pointerDiv.height) / 2;
+    createParticle(pointerDiv, x, y);
+    // setPointerDiv(null);
+  }, [canvas, pointerDiv, createParticle]);
 
   useEffect(() => {
     if (!canvas) return;
@@ -214,7 +266,19 @@ export default function ScatterCanvas(props: Props) {
   return (
     <>
       <canvas ref={canvasRef} onClick={handleCanvas}></canvas>
-      {pointerDiv && <PointerDiv onClick={handlePointerDiv} $width={pointerDiv.width} $height={pointerDiv.height} />}
+      {pointerDiv && (
+        <PointerDiv
+          onMouseDown={handlePointerDiv}
+          onMouseUp={() => {
+            canvas?.animate(() => {
+              return animation(true);
+            });
+            setPointerDiv(null);
+          }}
+          $width={pointerDiv.width}
+          $height={pointerDiv.height}
+        />
+      )}
     </>
   );
 }
--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -1,11 +1,21 @@
 import ScatterCanvas from "../components/ScatterCanvas";
 
-const text = "인트로 페이지 입니다."
-export default function IntroPage() {
+const text = [
+  "여기는 각자의 수심을 적는 웹페이지 입니다.",
+  "당신의 수심을 적어주세요",
+  "",
+  "당신이 불안을 적는 동안 쉰 숨의 기록은 가시화되어 삶의 역동성으로 기록됩니다.",
+  "",
+  "수심이 차오르고 개개인의 수심이 모여 연결됩니다.",
+  "그러다 한곳으로 흘러 들어가는 경험을 하게 됩니다.",
+  "",
+  "반드시 마이크 사용 가능한 환경을 준비해주세요",
+];
 
+export default function IntroPage() {
   return (
     <>
-      <ScatterCanvas text={text}/>
+      <ScatterCanvas text={text} />
     </>
   );
 }
