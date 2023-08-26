f09f0f0fd9c82ad0c841c79662f889d9d85f7992
윤현정 <yoonhj97@naver.com>
2023-08-26 20:17:41 +0900

    feat(MainPage): level 별 화면 넘어가게 처리

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,49 @@
-import { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import LinearDataCanvas from "../components/LinearDataCanvas";
+import styled from "styled-components";
+import LinearDataCanvas, { LinearDataCanvasHandle } from "../components/LinearDataCanvas";
+import ScatterCanvas from "../components/ScatterCanvas";
+
+const AnimationDiv = styled.div`
+  width: 60%;
+  min-height: 200px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transition: all 3s ease-out;
+`;
+
+const MyInput = styled.textarea`
+  width: 100%;
+  height: 200px;
+  border: 1px solid #ffffff;
+  background: rgba(0, 0, 0, 0.7);
+  padding: 10px;
+  margin: 12px 0;
+  outline: none;
+`;
+
+const MyButton = styled.button`
+  padding: 10px 12px;
+  text-align: center;
+  border: 1px solid #ffffff;
+  background: rgba(0, 0, 0, 0.7);
+  float: right;
+`;
+
+const text = [
+  "당신이 적은 수심은 사라졌습니다.",
+  "배수된 감정과 물은 모두 섞여 어딘가로 흘렀습니다.",
+  "",
+  "하지만, 당신이 수심을 적을때 남긴 숨의 기록은",
+  "당신에게 전송되었습니다.",
+  "",
+  "이 사이트에는 24간 동안은 당신의 수심이 기록되지만,",
+  "그 이후에는 영구적으로 삭제됩니다.",
+  "",
+  "다른이들의 수심을 보고 싶다면 아카이브 버튼을 클릭하세요.",
+];
 
 // const AudioContext = window.AudioContext || window.webkitAudioContext;
 const AudioContext = window.AudioContext;
@@ -9,7 +52,10 @@ let bufferLength: number;
 let dataArray: Uint8Array;
 
 export default function MainPage() {
+  const canvasRef = useRef<LinearDataCanvasHandle>(null);
   const [analyser, setAnalyser] = useState<AnalyserNode | null>(null);
+  const [level, setLevel] = useState(0);
+  const [isLast, setIsLast] = useState(false);
   const navigate = useNavigate();
 
   const getDomainData = useCallback(() => {
@@ -28,25 +74,8 @@ export default function MainPage() {
         audioCtx = new AudioContext();
 
         const analyser = audioCtx.createAnalyser();
-        // const distortion = audioCtx.createWaveShaper();
 
         setAnalyser(analyser);
-
-        // const makeDistortionCurve = (amount: number) => {
-        //   const k = typeof amount === "number" ? amount : 50;
-        //   const n_samples = 44100;
-        //   const curve = new Float32Array(n_samples);
-        //   const deg = Math.PI / 180;
-        //   let i = 0;
-        //   let x;
-        //   for (; i < n_samples; ++i) {
-        //     x = (i * 2) / n_samples - 1;
-        //     curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
-        //   }
-        //   return curve;
-        // };
-
-        // distortion.curve = makeDistortionCurve(50);
 
         analyser.fftSize = 256;
         analyser.smoothingTimeConstant = 0.85;
@@ -71,16 +100,78 @@ export default function MainPage() {
     void getMediaStream();
   }, [getMediaStream]);
 
+  useEffect(() => {
+    switch (level) {
+      case 0:
+        break;
+      case 1:
+        setTimeout(() => {
+          canvasRef.current?.stopAnimation();
+          setLevel((v) => v + 1);
+        }, 5000);
+        break;
+      case 2:
+        break;
+      case 3:
+        setTimeout(() => {
+          setLevel((v) => v + 1);
+        }, 5000);
+        break;
+      case 4:
+        setIsLast(true);
+        break;
+    }
+  }, [level]);
+
   return (
-    <div>
-      {analyser && <LinearDataCanvas getDomainData={getDomainData} />}
-      {/* <button
-        onClick={() => {
-          navigate("/archive");
-        }}
-      >
-        Go Archive
-      </button> */}
-    </div>
+    <>
+      {isLast ? (
+        <ScatterCanvas
+          text={text}
+          afterAnimationFunc={() => {
+            setLevel(0);
+            setIsLast(false);
+          }}
+        />
+      ) : (
+        <>
+          {analyser && <LinearDataCanvas ref={canvasRef} getDomainData={getDomainData} />}
+          <div className="w-full h-full relative">
+            <AnimationDiv style={{ opacity: level === 0 ? 1 : 0, visibility: level === 0 ? "visible" : "hidden" }}>
+              <p className="text-center">당신의 수심을 적어주세요</p>
+              <MyInput />
+              <MyButton
+                onClick={() => {
+                  setLevel(1);
+                }}
+              >
+                전송하기
+              </MyButton>
+            </AnimationDiv>
+            <AnimationDiv style={{ opacity: level === 2 ? 1 : 0, visibility: level === 2 ? "visible" : "hidden" }}>
+              <p className="text-center">당신의 숨의 기록을 전송하시겠습니까 ?</p>
+              <MyInput placeholder="이메일을 입력해주세요." />
+              <MyButton
+                onClick={() => {
+                  setLevel(3);
+                }}
+              >
+                아니요
+              </MyButton>
+              <MyButton>전송하기</MyButton>
+            </AnimationDiv>
+            {level === 0 && (
+              <button className="fixed right-4 bottom-4"
+                onClick={() => {
+                  navigate("/archive");
+                }}
+              >
+                Go Archive
+              </button>
+            )}
+          </div>
+        </>
+      )}
+    </>
   );
 }
