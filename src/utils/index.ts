5f67e4f6a60853d243779cef8d1b2ec752980a23
윤현정 <yoonhj97@naver.com>
2023-08-26 13:38:20 +0900

    refactor(util): getRandomNum isInt boolean 인자 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
-export const getRandomInt = (min: number, max: number): number => {
-  return Math.random() * (max - min) + min;
+export const getRandomNum = (min: number, max: number, isInt = false): number => {
+  const num = Math.random() * (max - min) + min;
+  if (isInt) Math.floor(num);
+  return Number(num.toFixed(2));
 };
