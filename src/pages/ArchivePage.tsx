83c6a23656fe897b8cea2e49b7f9d177b7db8763
윤현정 <yoonhj97@naver.com>
2023-09-04 18:17:34 +0900

    feat(ArchivePage): 24시간 이내 데이터 불러오기 제한 & CardLayout 컴포넌트 추가

--- src/pages/ArchivePage.tsx
+++ src/pages/ArchivePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-
+import styled from "styled-components";
 import database from "../utils/firebase";
 import { get, query, limitToFirst, startAfter, orderByChild } from "firebase/database";
 
@@ -11,6 +11,47 @@ type listType = {
   date: number;
   text: string;
 };
+
+const CardLayout = styled.div`
+  position: relative;
+  height: 18.750rem;
+
+  .front,
+  .back {
+    width: 100%;
+    height: 100%;
+    border: 1px solid #ffffff;
+    border-radius: 10px;
+    padding: 5px;
+    transition: all 0.8s ease-out;
+  }
+
+  .front {
+    background: #000000;
+  }
+
+  .back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transform: rotateY(180deg);
+    background: #ffffff;
+    color: #000000;
+  }
+
+  &:hover {
+    .front {
+      transform: rotateY(-180deg);
+      z-index: -2;
+    }
+    
+    .back {
+      transform: rotateY(0deg);
+      z-index: 0;
+    }
+  }
+`;
 
 const size = 5;
 export default function ArchivePage() {
@@ -28,10 +69,13 @@ export default function ArchivePage() {
   const getSusimList = useCallback(
     (isFirst = false) => {
       if (!isLast) {
-        console.log("in");
         const db = database("susims");
+
+        // 24시간 이전 데이터부터 가져오기
+        const startDate = isFirst ? new Date().getTime() - 1000 * 60 * 60 * 24 : getLastData();
         const queryList = [orderByChild("date"), limitToFirst(size)];
-        if (!isFirst) queryList.push(startAfter(getLastData(), "date"));
+
+        queryList.push(startAfter(startDate, "date"));
 
         get(query(db, ...queryList))
           .then((snapshot) => {
@@ -67,21 +111,31 @@ export default function ArchivePage() {
 
   return (
     <div>
-      archivePage
       <br />
       <button className="fixed right-4 top-4" onClick={() => navigate("/main")}>
         나가기
       </button>
-      {list?.map((v, idx) => {
-        return <div key={idx}>{v.text}</div>;
-      })}
-      <button
+      <div className="grid gap-4 grid-cols-5 grid-rows-2 w-11/12">
+        {list?.map((v, idx) => {
+          return (
+            <CardLayout>
+              <div className="front" key={`fron-${idx}`}>
+                {new Date(v.date).toString()}
+              </div>
+              <div className="back" key={`back-${idx}`}>
+                {v.text}
+              </div>
+            </CardLayout>
+          );
+        })}
+      </div>
+      {/* <button
         onClick={() => {
           getSusimList();
         }}
       >
         test
-      </button>
+      </button> */}
     </div>
   );
 }
