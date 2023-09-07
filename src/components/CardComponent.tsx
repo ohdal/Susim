624d8b4d6ca57ffeb60a28fa14eeba57cacac7ae
윤현정 <yoonhj97@naver.com>
2023-09-07 17:47:01 +0900

    feat(Archive): infinite scroll 구현

---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import LinearDataCanvas, { lineType, LinearDataCanvasHandle } from "../components/LinearDataCanvas";
+import { debounce } from "../utils";
+
+type CardProps = {
+  // data: lineType;
+  data: string;
+  // canvasInfo: { width: number; height: number };
+  canvasInfo: string;
+  text: string;
+};
+
+const CardLayout = styled.div`
+  position: relative;
+  height: 18.75rem;
+
+  .front,
+  .back {
+    width: 100%;
+    height: 100%;
+    border: 1px solid #ffffff;
+    border-radius: 10px;
+    padding: 5px;
+    transition: all 0.8s ease-out;
+  }
+
+  .front {
+    contain: content;
+    background: #000000;
+  }
+
+  .back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transform: rotateY(180deg);
+    background: #ffffff;
+    color: #000000;
+  }
+
+  &:hover {
+    .front {
+      transform: rotateY(-180deg);
+      z-index: -2;
+    }
+
+    .back {
+      transform: rotateY(0deg);
+      z-index: 0;
+    }
+  }
+`;
+
+const CardComponent = (props: CardProps) => {
+  const { data, text, canvasInfo } = props;
+  const layoutRef = useRef<HTMLDivElement | null>(null);
+  const canvasRef = useRef<LinearDataCanvasHandle | null>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current || !layoutRef.current) return;
+
+    const myResize = debounce(() => {
+      const width = (layoutRef.current?.clientWidth as number) - 10;
+      const height = (layoutRef.current?.clientHeight as number) - 10;
+      canvasRef.current?.canvasResize(width, height);
+      canvasRef.current?.currentDraw(
+        JSON.parse(data) as lineType,
+        JSON.parse(canvasInfo) as { width: number; height: number }
+      );
+    }, 300);
+
+    myResize();
+
+    window.addEventListener("resize", myResize);
+
+    return () => {
+      window.removeEventListener("resize", myResize);
+    };
+  }, [data, canvasInfo]);
+
+  return (
+    <CardLayout ref={layoutRef}>
+      <div className="front">
+        <LinearDataCanvas ref={canvasRef} />
+      </div>
+      <div className="back">{text}</div>
+    </CardLayout>
+  );
+};
+
+function cardPropsAreEqual(prev: CardProps, next: CardProps) {
+  return prev.data === next.data;
+}
+
+export default React.memo(CardComponent, cardPropsAreEqual);
--- src/pages/ArchivePage.tsx
+++ src/pages/ArchivePage.tsx
@@ -1,16 +1,9 @@
 import { useEffect, useState, useCallback, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import LinearDataCanvas, { lineType, LinearDataCanvasHandle } from "../components/LinearDataCanvas";
-import { debounce } from "../utils";
 import database from "../utils/firebase";
 import { get, query, limitToFirst, startAfter, orderByChild } from "firebase/database";
 
-type CardProps = {
-  data: lineType;
-  canvasInfo: { width: number; height: number };
-  text: string;
-};
+import CardComponent from "../components/CardComponent";
 
 type listType = {
   data: string;
@@ -19,93 +12,20 @@ type listType = {
   date: number;
 };
 
-const CardLayout = styled.div`
-  position: relative;
-  height: 18.75rem;
-
-  .front,
-  .back {
-    width: 100%;
-    height: 100%;
-    border: 1px solid #ffffff;
-    border-radius: 10px;
-    padding: 5px;
-    transition: all 0.8s ease-out;
-  }
-
-  .front {
-    contain: content;
-    background: #000000;
-  }
-
-  .back {
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    transform: rotateY(180deg);
-    background: #ffffff;
-    color: #000000;
-  }
-
-  &:hover {
-    .front {
-      transform: rotateY(-180deg);
-      z-index: -2;
-    }
-
-    .back {
-      transform: rotateY(0deg);
-      z-index: 0;
-    }
-  }
-`;
-
-function CardComponent(props: CardProps) {
-  const { data, text, canvasInfo } = props;
-  const layoutRef = useRef<HTMLDivElement | null>(null);
-  const canvasRef = useRef<LinearDataCanvasHandle | null>(null);
-
-  useEffect(() => {
-    if (!canvasRef.current || !layoutRef.current) return;
-
-    const myResize = debounce(() => {
-      const width = (layoutRef.current?.clientWidth as number) - 10;
-      const height = (layoutRef.current?.clientHeight as number) - 10;
-      canvasRef.current?.canvasResize(width, height);
-      canvasRef.current?.currentDraw(data, canvasInfo);
-    }, 300);
-
-    myResize();
-
-    window.addEventListener("resize", myResize);
-
-    return () => {
-      window.removeEventListener("resize", myResize);
-    };
-  }, [data, canvasInfo]);
-
-  return (
-    <CardLayout ref={layoutRef}>
-      <div className="front">
-        <LinearDataCanvas ref={canvasRef} />
-      </div>
-      <div className="back">{text}</div>
-    </CardLayout>
-  );
-}
-
-const size = 10;
+const size = 5;
 export default function ArchivePage() {
   const navigate = useNavigate();
+  const outterRef = useRef<HTMLDivElement>(null);
+  const innerRef = useRef<HTMLDivElement>(null);
   const [list, setList] = useState<listType[] | null>(null);
   const [isLast, setIsLast] = useState(false);
 
-  const getLastData = useCallback((): number => {
-    if (!list) return 0;
-
-    const idx = list.length - 1;
-    return list[idx].date;
+  const getStartDate = useCallback((): number => {
+    if (!list) return new Date().getTime() - 1000 * 60 * 60 * 24 * 14;
+    else {
+      const idx = list.length - 1;
+      return list[idx].date;
+    }
   }, [list]);
 
   const getSusimList = useCallback(
@@ -115,7 +35,7 @@ export default function ArchivePage() {
 
         // 24시간 이전 데이터부터 가져오기
         // const startDate = isFirst ? new Date().getTime() - 1000 * 60 * 60 * 24 : getLastData();
-        const startDate = isFirst ? new Date().getTime() - 1000 * 60 * 60 * 24 * 7 : getLastData();
+        const startDate = getStartDate();
         const queryList = [orderByChild("date"), limitToFirst(size)];
 
         queryList.push(startAfter(startDate, "date"));
@@ -129,12 +49,9 @@ export default function ArchivePage() {
               if (newArr.length < size) setIsLast(true);
 
               if (isFirst) {
-                console.log(newArr);
                 setList(newArr);
               } else {
-                setList((v) => {
-                  return v ? v.concat(newArr) : v;
-                });
+                setList((list as listType[]).concat(newArr));
               }
             } else {
               setIsLast(true);
@@ -145,34 +62,49 @@ export default function ArchivePage() {
           });
       }
     },
-    [getLastData, isLast]
+    [getStartDate, isLast, list]
+  );
+
+  const handleScroll = useCallback(
+    (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+      const clientHeight = (e.target as HTMLDivElement).clientHeight;
+      const scrollHeight = (e.target as HTMLDivElement).scrollHeight;
+      const scrollTop = (e.target as HTMLDivElement).scrollTop;
+
+      if (scrollHeight - clientHeight === scrollTop) getSusimList();
+    },
+    [getSusimList]
   );
 
   useEffect(() => {
     if (!list) getSusimList(true);
+    else {
+      if (outterRef.current && innerRef.current && outterRef.current.clientHeight >= innerRef.current.clientHeight)
+        getSusimList();
+    }
   }, [getSusimList, list]);
 
   return (
-    <div className="p-10 w-full h-full">
+    <div className="p-16 w-full h-full">
       <button className="fixed right-4 top-4" onClick={() => navigate("/main")}>
         나가기
       </button>
-      <div className="w-full h-full grid overflow-auto">
-        <div className="grid gap-4 grid-rows-2 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 items-center">
+      <div
+        className="w-full h-full grid overflow-auto"
+        ref={outterRef}
+        onScroll={(e) => {
+          handleScroll(e);
+        }}
+      >
+        <div
+          className="grid gap-4 grid-rows-2 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 items-center"
+          ref={innerRef}
+        >
           {list?.map((v, idx) => {
-            return (
-              <CardComponent data={JSON.parse(v.data)} text={v.text} canvasInfo={JSON.parse(v.canvasInfo)} key={idx} />
-            );
+            return <CardComponent data={v.data} text={v.text} canvasInfo={v.canvasInfo} key={idx} />;
           })}
         </div>
       </div>
-      {/* <button
-        onClick={() => {
-          getSusimList();
-        }}
-      >
-        test
-      </button> */}
     </div>
   );
 }
