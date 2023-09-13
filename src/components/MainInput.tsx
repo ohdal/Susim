e5b66d13aec86eb14757761e7de6271260c69b79
윤현정 <yoonhj97@naver.com>
2023-09-13 16:20:18 +0900

    chore(MainInput): 현재 입력값 길이 표시

--- src/components/MainInput.tsx
+++ src/components/MainInput.tsx
@@ -5,9 +5,18 @@ const InputComp = styled.input`
   width: 100%;
   border: 1px solid #ffffff;
   background: rgba(0, 0, 0, 0.7);
-  padding: 10px 20px;
-  margin: 12px 0;
+  padding: 10px 120px 10px 20px;
   outline: none;
+`;
+
+const TextLengthDiv = styled.div`
+  width: 120px;
+  position: absolute;
+  padding-right: 20px;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  text-align: right;
 `;
 
 type Props = {
@@ -15,6 +24,7 @@ type Props = {
   placeholder?: string;
   max?: number;
   min?: number;
+  visibleCount: boolean;
   changeEventHandle?: <Params extends any[]>(...args: Params) => void;
 };
 
@@ -25,7 +35,7 @@ export interface MainInputHandle {
 
 const MAX_DEFAULT = 100;
 const MainInput = forwardRef<MainInputHandle, Props>((props, ref) => {
-  const { name, placeholder, changeEventHandle, max } = props;
+  const { name, placeholder, changeEventHandle, max, visibleCount } = props;
   const [text, setText] = useState("");
 
   useImperativeHandle(ref, () => ({
@@ -36,18 +46,21 @@ const MainInput = forwardRef<MainInputHandle, Props>((props, ref) => {
   }));
 
   return (
-    <InputComp
-      name={name}
-      placeholder={placeholder}
-      value={text}
-      onChange={(e) => {
-        const text = e.target.value;
-        const size = max ? max : MAX_DEFAULT;
-        if (text.length > size) setText(text.slice(0, size));
-        else setText(e.target.value);
-        if (changeEventHandle) changeEventHandle(e.target.value, size);
-      }}
-    />
+    <div className="my-3 relative">
+      <InputComp
+        name={name}
+        placeholder={placeholder}
+        value={text}
+        onChange={(e) => {
+          const text = e.target.value;
+          const size = max ? max : MAX_DEFAULT;
+          if (text.length > size) setText(text.slice(0, size));
+          else setText(e.target.value);
+          if (changeEventHandle) changeEventHandle(e.target.value);
+        }}
+      />
+      {visibleCount && <TextLengthDiv>{`${text.length} / ${max ? max : MAX_DEFAULT}`}</TextLengthDiv>}
+    </div>
   );
 });
 
