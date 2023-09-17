94a52703f86e17de93c9eff4ccda0d5e42fb20fe
윤현정 <yoonhj97@naver.com>
2023-09-17 18:17:45 +0900

    fix(linearData): 화면 resize 이벤트 발생 시 opacity 랜덤값으로 들어가 수심이 랜덤으로 그려지는 현상 수정 (opacity 값도 저장)

--- src/components/LinearDataCanvas.tsx
+++ src/components/LinearDataCanvas.tsx
@@ -15,68 +15,26 @@ export interface LinearDataCanvasHandle {
   fillUp: (v: string) => void;
 }
 
-type dotDataType = { x: number; y: number };
+type dotDataType = { x: number; y: number; opacity?: number };
 type dotGroupType = dotDataType[];
 export type lineType = dotGroupType[];
 type lineInfoType = { yPos: number; per?: number; particle?: number; large?: boolean };
 
-class Queue {
-  private arr: number[];
-  private buffer: number;
+// class Particle {
+//   constructor() {
 
-  constructor(bufferLength: number, data: Uint8Array) {
-    this.arr = this.filterData(data);
-    this.buffer = bufferLength;
-  }
+//   }
 
-  setData(data: Uint8Array) {
-    this.arr = this.filterData(data);
-    return;
+//   update() {
 
-    if (this.arr.length >= this.buffer * 2) return;
+//   }
 
-    // const filter = this.filterData(data);
-    // if (filter.length === 0) return;
+//   draw() {
 
-    // const last = this.arr[this.arr.length - 1];
-    // const first = filter[0];
-    // const diff = Math.abs(last - first);
-    // const arrCor = [];
+//   }
+// }
 
-    // if (diff > 10) {
-    //   const value = Math.floor(diff / 5);
-
-    //   for (let i = 1; i <= 4; i++) {
-    //     if (last > first) arrCor.push(last - value * i);
-    //     else arrCor.push(last + value * i);
-    //   }
-    // }
-
-    // this.arr.push(...arrCor, ...filter);
-  }
-
-  getLength(): number {
-    return this.arr.length;
-  }
-
-  getData(idx: number): number {
-    const value = this.arr[idx];
-
-    return value;
-  }
-
-  shiftData() {
-    this.arr.shift();
-    this.arr.shift();
-  }
-
-  filterData(data: Uint8Array): number[] {
-    // return Array.from(data).filter((v) => !(v < 129 && v > 126));
-    return Array.from(data);
-  }
-}
-
-let queue: Queue | null = null;
+let particles: number[] | null = null;
 let lineArr: lineType = [];
 const lineInfoArr: lineInfoType[] = [
   { yPos: -122 },
@@ -132,11 +90,15 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
     return { data: lineArr, width, height };
   }, [canvas]);
 
-  const getImageData = useCallback((): string |  null => {
+  const getImageData = useCallback((): string | null => {
     if (!canvas) return null;
 
     return canvas.element.toDataURL();
   }, [canvas]);
+
+  const getRandomOpacity = useCallback((isOrigin = false): number => {
+    return isOrigin ? getRandomNum(0.7, 1) : getRandomNum(0.2, 1);
+  }, []);
 
   const saveDot = useCallback((arr: dotGroupType, data: dotDataType, info: lineInfoType) => {
     const { yPos, per, particle, large } = info;
@@ -146,20 +108,21 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
     const standard = isOrigin ? 1000 : per ? 10 * per : 300;
 
     if (num <= standard && arr) {
-      arr.push(data);
+      arr.push(Object.assign(data, { opacity: getRandomOpacity(isOrigin) }));
 
       if (isOrigin) {
-        arr.push({ x: data.x + getRandomNum(1, 5), y: data.y });
-        arr.push({ x: data.x, y: data.y + getRandomNum(1, 5) });
+        arr.push({ x: data.x + getRandomNum(1, 5), y: data.y, opacity: getRandomOpacity(isOrigin) });
+        arr.push({ x: data.x, y: data.y + getRandomNum(1, 5), opacity: getRandomOpacity(isOrigin) });
       } else {
         const size = particle ? particle : 2;
 
         for (let i = 0; i < size; i++) {
-          arr.push({ x: data.x + getRandomNum(10, 20), y: data.y + getRandomNum(5, large ? 20 : 10) });
+          const opacity = getRandomOpacity(isOrigin);
+          arr.push({ x: data.x + getRandomNum(10, 20), y: data.y + getRandomNum(5, large ? 20 : 10), opacity });
         }
       }
     }
-  }, []);
+  }, [getRandomOpacity]);
 
   const drawDot = useCallback(
     (arr: dotGroupType, info: lineInfoType, ratio: { x: number; y: number }, color = "#ffffff") => {
@@ -167,17 +130,15 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
 
       const { yPos } = info;
       const { r, g, b } = hexToRgb(color) || { r: 255, g: 255, b: 255 };
-      const isOrigin = yPos === 0;
       const ctx = canvas.ctx as CanvasRenderingContext2D;
 
       if (arr) {
         const v = Math.floor(1 / ratio.x);
         for (let i = 0; i < arr.length; i += v > 1 ? v : 1) {
-          const { x, y } = arr[i];
-          const opacity = isOrigin ? getRandomNum(0.7, 1) : getRandomNum(0.2, 1);
+          const { x, y, opacity } = arr[i];
 
           ctx.beginPath();
-          ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+          ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${opacity as number})`;
           ctx.arc((x + (yPos < 0 ? 10 : 0)) * ratio.x, (y - yPos) * ratio.y, 1, 0, (Math.PI / 180) * 360);
           ctx.fill();
           ctx.closePath();
@@ -217,16 +178,7 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
     const ctx = canvas.ctx as CanvasRenderingContext2D;
     const { bufferLength, dataArray } = getDomainData();
 
-    if (!queue) {
-      queue = new Queue(bufferLength, dataArray);
-
-      // setInterval(() => {
-      //   const { dataArray } = getDomainData();
-      //   queue?.setData(dataArray);
-      // }, 1000);
-    } else {
-      queue.setData(dataArray);
-    }
+    particles = Array.from(dataArray);
 
     lineArr = [];
 
@@ -241,8 +193,8 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
     const sliceWidth = (canvas.CANVAS_WIDTH * 1.0) / bufferLength;
     let x = 0;
 
-    for (let i = 0; i < queue.getLength(); i++) {
-      const data = queue.getData(i);
+    for (let i = 0; i < particles.length; i++) {
+      const data = particles[i];
       const v = data / 128.0;
       const y = (v * canvas.CANVAS_HEIGHT) / 1.4 + yPosCount;
 
@@ -250,10 +202,7 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
         for (let i = 0; i < lineInfoArr.length; i++) {
           lineArr.push([]);
         }
-        // ctx.moveTo(x, y);
       } else {
-        // ctx.lineTo(x, y);
-
         for (let i = 0; i < lineArr.length; i++) {
           saveDot(lineArr[i], { x, y }, lineInfoArr[i]);
         }
@@ -262,13 +211,10 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
       x += sliceWidth;
     }
 
-    // ctx.lineTo(x, canvas.CANVAS_HEIGHT / 2);
     ctx.stroke();
     ctx.closePath();
 
     currentDraw(lineArr);
-
-    // queue.shiftData();
   }, [canvas, getDomainData, saveDot, currentDraw]);
 
   useEffect(() => {
@@ -295,7 +241,7 @@ const LinearDataCanvas = forwardRef<LinearDataCanvasHandle, Props>((props, ref) 
     if (canvasRef.current) {
       setCanvas(new Canvas(canvasRef.current));
 
-      queue = null;
+      particles = null;
       lineArr = [];
       yPosCount = 0;
     }
